df4f083f75fae99667bf176c80f8314bbd7f3e9f
Yasmeen Hart <hartyasmeen@gmail.com>
2022-10-22 09:59:09 -0400

    Initial Patient class

---
+++ hackathon-azalea-2022/src/ver1/Patient.java
@@ -0,0 +1,13 @@
+package ver1;
+
+public class Patient {
+
+	String name;
+	String bday;
+	int age;
+	public Patient(String name, String bday, int age) {
+		this.name = name;
+		this.bday = bday;
+		this.age = age;
+	}
+}
