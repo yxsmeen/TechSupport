8b946d4317ee252dcf4e17ae46e07d873559e1dd
Prospero Castellanos <Prospero493@gmail.com>
2022-10-22 10:58:29 -0400

    Update Storage.java

--- hackathon-azalea-2022/src/ver1/Storage.java
+++ hackathon-azalea-2022/src/ver1/Storage.java
@@ -1,5 +1,42 @@
 package ver1;
 
+import java.util.ArrayList;
+
 public class Storage {
-	String s = "";
+	ArrayList<Patient>AccountList = new ArrayList<Patient>();
+	public Storage(Patient p){
+		
+	}
+	public void addPatient(Patient p) {
+		AccountList.add(p);
+	}
+	public void removePatient(Patient p) {
+		AccountList.remove(p);
+	}
+	public String returnNumList() {
+		String s  ="There are " + AccountList.size() + " many patients in our system";
+		return s;
+	}
+	public String printList() {
+		String s = "";
+		for(int i =0;i<AccountList.size();i++) {
+			s+= "Patient name is " + AccountList.get(i).name + ", age is " + AccountList.get(i).age + 
+					" finally their birthdate is " + AccountList.get(i).bday + "\n";
+		}
+		return s;
+	}
+	public void resetSystem() {
+		AccountList.clear();
+	}
+	public String checkPatientStatus(Patient p) {
+		String s = "";
+		for(int i =0;i<AccountList.size();i++) {
+			if (p.equals(AccountList.get(i))) {
+				s += "Patient status is " + p;
+				// insert their status here once method exists 
+			}
+		}
+		return s;
+	
+	}
 }
